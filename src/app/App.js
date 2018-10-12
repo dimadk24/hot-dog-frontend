47f09a493994943db142a9ad49488e40002736de
DimaDK <DimaKrutDK@ya.ru>
2018-10-12 11:29:37 +0300

    fix: add base url, so gh pages deploy works

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ import HomePage from './components/HomePage/HomePage'
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Fragment>
                     <Navigation />
                     <Switch>
