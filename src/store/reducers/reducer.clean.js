32f2a639b1e73a2632ce56ab7a43df0aecade060
Dima Baranov <vajnoe-dima@mail.ru>
2018-10-13 13:33:46 +0300

    add some data structure

--- src/store/reducers/reducer.clean.js
+++ src/store/reducers/reducer.clean.js
@@ -3,3 +3,32 @@ export const CLEAN_GROUPS = 'groups/CLEAN_GROUPS'
 export const DELETE_GROUPS = 'groups/DELETE_GROUPS'
 export const SELECT_GROUP = 'groups/SELECT_GROUP'
 export const ADD_GROUP = 'groups/ADD_GROUP'
+export const START_COUNTING_DELETED_PEOPLE =
+    'groups/START_COUNTING_DELETED_PEOPLE'
+export const STOP_COUNTING_DELETED_PEOPLE =
+    'groups/STOP_COUNTING_DELETED_PEOPLE'
+
+const groupID = 1
+const initialState = {
+    groups: {
+        [groupID]: {
+            groupName: 'My group',
+            peopleCount: 500000,
+            deletedPeople: 120,
+            isLoading: true
+        }
+    }
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_GROUP:
+            const groupToAdd = action.payload
+            return {
+                ...state,
+                groups: [...state.groups, groupToAdd]
+            }
+        default:
+            return state
+    }
+}
