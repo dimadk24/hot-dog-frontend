b5bb7fe4f3027be69d8870364ee4fe0feeea762b
DimaDK <DimaKrutDK@ya.ru>
2018-10-14 17:07:42 +0300

    draft: code for getting user publics

--- src/app/components/CleanPage/CleanPage.js
+++ src/app/components/CleanPage/CleanPage.js
@@ -3,17 +3,57 @@ import PanelControl from './PanelControl/PanelControl'
 import PublicGroup from './PublicGroup/PublicGroup'
 
 class CleanPage extends Component {
+    constructor(props) {
+        super(props)
+        /*global VK*/
+        VK.init(
+            () => {
+                VK.api(
+                    'groups.get',
+                    {
+                        filter: 'moder',
+                        extended: '1',
+                        fields: 'photo_100',
+                        v: '5.85'
+                    },
+                    (data) => {
+                        const publics = convertPublicsFromVkFormat(
+                            data.response.items
+                        )
+                        const publics_count = data.response.count
+                        console.log(`Got ${publics_count} publics from VK:`)
+                        console.log(publics)
+                    }
+                )
+            },
+            () => {
+                console.log('VK API initialization failed')
+            },
+            '5.85'
+        )
+    }
+
     render() {
         return (
             <div className="clean">
                 <PanelControl />
                 <PublicGroup />
-                <div className='btn btn__add'>
-                    Добавить Сообщество
-                </div>
+                <div className="btn btn__add">Добавить Сообщество</div>
             </div>
         )
     }
 }
 
 export default CleanPage
+
+function convertPublicsFromVkFormat(array) {
+    function converter(item) {
+        // noinspection JSUnresolvedVariable
+        return {
+            ava: item.photo_100,
+            id: item.id,
+            name: item.name
+        }
+    }
+    return array.map(converter)
+}
