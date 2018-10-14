162acb135c4d44490e981b43a350570083bc263b
Dima Baranov <vajnoe-dima@mail.ru>
2018-10-14 14:50:06 +0300

    create root reducer

---
+++ src/store/index.js
@@ -0,0 +1,4 @@
+import {createStore, applyMiddleware} from "redux";
+import rootReducer from "./reducers/reducer.root";
+
+const store = createStore(rootReducer, )
---
+++ src/store/reducers/reducer.root.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux'
+import clean from './reducer.clean'
+
+export default combineReducers({
+    clean
+})
