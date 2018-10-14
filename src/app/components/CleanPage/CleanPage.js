7c2acd29c0fcdefe5096077ede5b18d6fa5a76af
Dima Baranov <vajnoe-dima@mail.ru>
2018-10-14 15:06:12 +0300

    connect clean page to redux and render groups

--- src/app/components/CleanPage/CleanPage.js
+++ src/app/components/CleanPage/CleanPage.js
@@ -1,19 +1,34 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import PanelControl from './PanelControl/PanelControl'
 import PublicGroup from './PublicGroup/PublicGroup'
+import {bindActionCreators} from 'redux'
 
 class CleanPage extends Component {
+    renderGroups = (groups) => {
+        return Object.keys(groups).map((key, i) => (
+            <PublicGroup {...groups[key]} key={i} />
+        ))
+    }
     render() {
+        const {groups} = this.props
         return (
             <div className="clean">
                 <PanelControl />
-                <PublicGroup />
-                <div className='btn btn__add'>
-                    Добавить Сообщество
-                </div>
+                {groups && this.renderGroups(groups)}
+                <div className="btn btn__add">Добавить Сообщество</div>
             </div>
         )
     }
 }
 
-export default CleanPage
+const mapStateToProps = ({clean}) => ({
+    groups: clean.groups
+})
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch)
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CleanPage)
