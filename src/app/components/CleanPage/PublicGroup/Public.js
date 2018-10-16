098418f41c7c57a846548de97d0cba12b16fe530
DimaDK <DimaKrutDK@ya.ru>
2018-10-16 21:43:59 +0300

    feat: getting clean tasks data

--- src/app/components/CleanPage/PublicGroup/Public.js
+++ src/app/components/CleanPage/PublicGroup/Public.js
@@ -3,15 +3,58 @@ import {PublicInfo} from './PublicInfo'
 import {ProgressBar} from './ProgressBar'
 import {PublicAvatar} from './PublicAvatar'
 import './Public.css'
+import axios from 'axios'
 
 class Public extends Component {
     constructor(props) {
         super(props)
+        this.id = props.id
         this.state = {
             isCleaning: props.isCleaning,
-            cleanProgress: props.cleanTask.progress,
-            progressStatus: props.cleanTask.status
+            cleanProgress: props.cleanTask? props.cleanTask.progress : 0,
+            progressStatus: props.cleanTask ? props.cleanTask.status : ''
         }
+    }
+
+    componentWillMount() {
+        if (this.state.isCleaning) {
+            this.timerId = setInterval(async () => {
+                const cleanTasks = (await axios.get(
+                    'http://hot-dog.site/api/getCleanTasks',
+                    {
+                        params: {
+                            auth_access_token:
+                                '2c760152aad7cffcd688e7a0e7bc9b23904c522efeb98fc4f27c0ed89a084eb329aa3e23d2bf28d591844'
+                        }
+                    }
+                )).data
+                if (cleanTasks && cleanTasks.length) {
+                    for (const cleanTask of cleanTasks) {
+                        if (this.id === cleanTask.public_id) {
+                            this.setState({
+                                cleanProgress: cleanTask.progress,
+                                progressStatus: cleanTask.status
+                            })
+                            if (
+                                ['Возникла ошибка', 'Завершили'].includes(
+                                    cleanTask.status
+                                )
+                            )
+                                setTimeout(
+                                    () => this.setState({isCleaning: false}),
+                                    2500
+                                )
+                        }
+                    }
+                } else {
+                    this.setState({isCleaning: false})
+                }
+            }, 1000)
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.timerId) clearInterval(this.timerId)
     }
 
     render() {
@@ -19,8 +62,10 @@ class Public extends Component {
             <div className="public-wrapper">
                 <div className="public">
                     <div className="public__heading">
-                        <PublicAvatar url={this.props.avatar_url}/>
-                        <div className="public__heading__name">{this.props.name}</div>
+                        <PublicAvatar url={this.props.avatar_url} />
+                        <div className="public__heading__name">
+                            {this.props.name}
+                        </div>
                     </div>
                     <div className="public__info">
                         {this.state.isCleaning ? (
