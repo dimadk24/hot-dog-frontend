c6e339b693d69a88a2677ed28de90032f80524bd
DimaDK <DimaKrutDK@ya.ru>
2018-10-16 11:15:38 +0300

    chore: modal and code from VK dialog

---
+++ src/app/components/UI/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Modal extends React.Component {
+    render() {
+        return (
+            <div className="modal">
+                <div className='modal__wrapper'>
+                    <div className='modal__wrapper__content'>
+                        Modal
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Modal
