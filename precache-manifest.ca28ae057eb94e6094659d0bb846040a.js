8b6e392ab867798b0e0c8569e1d073ea9c21bedc
Dima Baranov <vajnoe-dima@mail.ru>
2018-12-04 19:38:28 +0300

    Updates

---
+++ precache-manifest.ca28ae057eb94e6094659d0bb846040a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "deadc55b0f3080aff049b42fdbb59064",
+    "url": "/static/media/fontello.deadc55b.svg"
+  },
+  {
+    "revision": "aba500a9187d00a5adf0b25675233f01",
+    "url": "/static/media/fontello.aba500a9.ttf"
+  },
+  {
+    "revision": "90b809ae1ce422cd4414669b2064404e",
+    "url": "/static/media/fontello.90b809ae.eot"
+  },
+  {
+    "revision": "31c8cd934f76c05ad188dbf79ac188e4",
+    "url": "/static/media/fontello.31c8cd93.woff"
+  },
+  {
+    "revision": "17b4028a14a7258ccc81f7055c04be3a",
+    "url": "/static/media/fontello.17b4028a.woff2"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "154f63028f196f4c19d3",
+    "url": "/static/js/main.154f6302.chunk.js"
+  },
+  {
+    "revision": "008702966e1c816f7c0a",
+    "url": "/static/js/1.00870296.chunk.js"
+  },
+  {
+    "revision": "154f63028f196f4c19d3",
+    "url": "/static/css/main.de2b8ba3.chunk.css"
+  },
+  {
+    "revision": "8e1388a97a9cf1170fd30ce3cf9a2be5",
+    "url": "/index.html"
+  }
+];
